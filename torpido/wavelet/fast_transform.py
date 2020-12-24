72a1f4fe46532793f290e5d5d3b84f0177be1999
AP-Atul <atulpatare99@gmail.com>
2020-12-24 20:26:37 +0530

    support for multiple channels

--- torpido/wavelet/fast_transform.py
+++ torpido/wavelet/fast_transform.py
@@ -3,44 +3,105 @@
 import numpy as np
 from torpido.wavelet.extension.base_transform import BaseTransform
 
-from torpido.wavelet.util import isPowerOf2, decomposeArbitraryLength, scalb, getExponent
+from torpido.wavelet.util import decomposeArbitraryLength, scalb, getExponent
 
 
 class FastWaveletTransform(BaseTransform):
+    """
+    Reads the dimensions of the input signal and calls
+    the respective functions of the Base Transform class
+    """
 
     def __init__(self, waveletName):
         super().__init__(waveletName)
 
     def waverec(self, arrHilbert, level=None):
+        """
+        Wavelet Reconstruction
+
+        Parameters
+        ----------
+        level: int
+            level for reconstruction
+        arrHilbert: array_like
+            input array in the Hilbert domain
+
+        Returns
+        -------
+        array_like
+            Time domain
+        """
+        dimensions = np.ndim(arrHilbert)
+
         # setting the max level
         if level is None:
             level = getExponent(len(arrHilbert))
 
-        # checking if the data is not of arbitrary length
-        # special cases only for 1D arrays
-        if not isPowerOf2(len(arrHilbert)):
-            # perform ancient egyptian decomposition
+        # for single dim data
+        if dimensions == 1:
+            # perform ancient egyptian reconstruction
             return self.__waveRecAncientEgyptian(arrHilbert, level)
 
-        else:
-            return self.waveRec1(arrHilbert, level)
+        # for two dim data
+        if dimensions == 2:
+            # perform ancient egyptian reconstruction
+            return self.__waveRecAncientEgyptian2(arrHilbert)
 
     def wavedec(self, arrTime, level=None):
+        """
+        Wavelet Decomposition
+
+        Parameters
+        ----------
+        level: int
+            level for decomposition
+        arrTime: array_like
+            input array in the Time domain
+
+        Returns
+        -------
+        array_like
+            Hilbert domain
+        """
+        dimensions = np.ndim(arrTime)
+
         # setting the max level
         if level is None:
             level = getExponent(len(arrTime))
 
-        # checking if the data is not of arbitrary length
-        # special cases only for 1D arrays
-        if not isPowerOf2(len(arrTime)):
+        # for two single data
+        if dimensions == 1:
             # perform ancient egyptian decomposition
             return self.__waveDecAncientEgyptian(arrTime, level)
 
-        # data of length power of 2
-        else:
-            return self.waveDec1(arrTime, level)
+        # for two dim data
+        if dimensions == 2:
+            # perform ancient egyptian decomposition
+            return self.__waveDecAncientEgyptian2(arrTime)
 
     def __waveDecAncientEgyptian(self, arrTime, level):
+        """
+        Wavelet decomposition for data of arbitrary length
+
+        References
+        ----------
+        The array is distributed by the length of the power of 2
+        and then wavelet decomposition is performed
+        Look into the utility.decomposeArbitraryLength function
+
+        for a data with length 42
+        Ex: 42 = 2^5, 2^3, 2^1   i.e. 32, 8, 2 lengths partitions are made
+
+        Parameters
+        ----------
+        arrTime: array_like
+            input array in the time domain
+
+        Returns
+        -------
+        array_like
+            hilbert domain array
+        """
         arrHilbert = list()
         powers = decomposeArbitraryLength(len(arrTime))
         offset = 0
@@ -59,6 +120,28 @@ class FastWaveletTransform(BaseTransform):
         return arrHilbert
 
     def __waveRecAncientEgyptian(self, arrHilbert, level):
+        """
+        Wavelet reconstruction for data of arbitrary length
+
+        References
+        ----------
+        The array is distributed by the length of the power of 2
+        and then wavelet decomposition is performed
+        Look into the utility.decomposeArbitraryLength function
+
+        for a data with length 42
+        Ex: 42 = 2^5, 2^3, 2^1   i.e. 32, 8, 2 lengths partitions are made
+
+        Parameters
+        ----------
+        arrHilbert: array_like
+            input array in the hilbert domain
+
+        Returns
+        -------
+        array_like
+            hilbert time array
+        """
         arrTime = list()
         powers = decomposeArbitraryLength(len(arrHilbert))
         offset = 0
@@ -76,3 +159,91 @@ class FastWaveletTransform(BaseTransform):
             offset += sliceIndex
 
         return arrTime
+
+    def __waveDecAncientEgyptian2(self, matTime):
+        """
+        Wavelet decomposition for data of arbitrary length (2D)
+
+        References
+        ----------
+        The array is distributed by the length of the power of 2
+        and then wavelet decomposition is performed
+        Look into the utility.decomposeArbitraryLength function
+
+        for a data with length 42
+        Ex: 42 = 2^5, 2^3, 2^1   i.e. 32, 8, 2 lengths partitions are made
+
+        Parameters
+        ----------
+        matTime: array_like
+            input 2D array in the time domain
+
+        Returns
+        -------
+        array_like
+            hilbert domain array
+        """
+        # shape
+        noOfRows = len(matTime)
+        noOfCols = len(matTime[0])
+
+        levelM = getExponent(noOfRows)
+        levelN = getExponent(noOfCols)
+
+        matHilbert = np.zeros(shape=(noOfRows, noOfCols))
+
+        # rows
+        for i in range(noOfRows):
+            # run the decomposition
+            matHilbert[i] = self.__waveDecAncientEgyptian(np.array(matTime[i], dtype=np.float_), levelN)
+
+        # cols
+        for j in range(noOfCols):
+            # run the decomposition
+            matHilbert[:, j] = self.__waveDecAncientEgyptian(np.array(matHilbert[:, j], dtype=np.float_), levelM)
+
+        return matHilbert
+
+    def __waveRecAncientEgyptian2(self, matHilbert):
+        """
+        Wavelet reconstruction for data of arbitrary length (2D)
+
+        References
+        ----------
+        The array is distributed by the length of the power of 2
+        and then wavelet decomposition is performed
+        Look into the utility.decomposeArbitraryLength function
+
+        for a data with length 42
+        Ex: 42 = 2^5, 2^3, 2^1   i.e. 32, 8, 2 lengths partitions are made
+
+        Parameters
+        ----------
+        matHilbert: array_like
+            input 2D array in the hilbert domain
+
+        Returns
+        -------
+        array_like
+            hilbert time array
+        """
+        noOfRows = len(matHilbert)
+        noOfCols = len(matHilbert[0])
+
+        # getting the levels
+        levelM = getExponent(noOfRows)
+        levelN = getExponent(noOfCols)
+
+        matTime = np.zeros(shape=(noOfRows, noOfCols))
+
+        # rows
+        for j in range(noOfCols):
+            # run the reconstruction on the row
+            matTime[:, j] = self.__waveRecAncientEgyptian(np.array(matHilbert[:, j], dtype=np.float_), levelM)
+
+        # cols
+        for i in range(noOfRows):
+            # run the reconstruction on the column
+            matTime[i] = self.__waveRecAncientEgyptian(np.array(matTime[i], dtype=np.float_), levelN)
+
+        return matTime
