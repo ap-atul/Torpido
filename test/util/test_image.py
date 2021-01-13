d58550f692128074b232de507764c20e13eb99c5
AP-Atul <atulpatare99@gmail.com>
2021-01-13 22:56:37 +0530

    added image utils tests

---
+++ test/util/test_image.py
@@ -0,0 +1,29 @@
+import unittest
+
+from torpido.util.image import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_image_resize(self):
+        white_image = np.full(
+            (1920, 720, 3),
+            255,
+            dtype=np.uint8
+        )
+        resized_image = resize(white_image, 500)
+        output_shape = (1333, 500, 3)
+        self.assertEqual(output_shape, resized_image.shape)
+
+    def test_image_resize_2(self):
+        white_image = np.full(
+            (500, 500),
+            255,
+            dtype=np.uint8
+        )
+        resized_image = resize(white_image, 500)
+        output_shape = (500, 500)
+        self.assertEqual(output_shape, resized_image.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
