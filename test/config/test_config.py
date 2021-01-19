38398f0a1528def9059ab7c7656e2af232699797
AP-Atul <atulpatare99@gmail.com>
2021-01-19 23:12:07 +0530

    changes to the config module, tests updated

--- test/config/test_config.py
+++ test/config/test_config.py
@@ -5,38 +5,20 @@ from torpido.config.config import *
 
 class ConfigTest(unittest.TestCase):
     def test_write(self):
-        config = Config()
-        d = config.read("WAVELET", str)
+        d = Config.WAVELET
 
         self.assertEqual("coif1", d)
 
     def test_read(self):
-        config = Config()
-        config.write("MYD", 30)
+        Config.THEME = "Crank"
 
-        self.assertEqual("30", config.read("MYD", str))
-
-    def test_read_dtype(self):
-        config = Config()
-        config.write("MYD", 30)
-
-        self.assertEqual(30, config.read("MYD", int))
+        self.assertEqual("Crank", Config.THEME)
 
     def test_check_dtype(self):
-        config = Config()
-        config.write("MYD", 30)
+        Config.THEME = "Crank"
 
-        self.assertEqual(int, type(config.read("MYD", int)))
-
-    def test_check_type(self):
-        config = Config()
-        config.write("Some", 30)
-
-        try:
-            config.read("Some", type(config))
-        except TypeError:
-            self.assertTrue(True)
-        self.assertFalse(False)
+        self.assertEqual("Crank", Config.THEME)
+        self.assertEqual(str, type(Config.THEME))
         
 
 if __name__ == '__main__':
